e50c31d597d90ed2c70328059a3c649ce6cb114c
s3778722 <s3778722@student.rmit.edu.au>
2021-05-10 19:45:24 +0800

    Testing some new functionality for the banner

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :product_variants
-  has_many :images
-  has_and_belongs_to_many :categories
+  has_many :product_variants, dependent: :destroy
+  has_many :images, dependent: :destroy
+  has_and_belongs_to_many :categories, dependent: :destroy
 end
