8d40b118397f53782e89656c1029890663373d6e
s3778722 <s3778722@student.rmit.edu.au>
2021-05-14 05:14:30 +0800

    New functionality for saved list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,28 @@
 class HomeController < ApplicationController
   def index
+    cookies.delete :saved_products
     @products = Product.all
+    #if form is submitted, add cookies
+    if params.include?(:product_id)
+      add_cookies(params[:product_id])
+      redirect_to root_path
+    end
+    @saved_list = cookies[:saved_products]
+
+    #to select the list item
+    #a = JSON.parse(cookies[:saved_productss])
+    #@output = a[2]
+  end
+
+  def add_cookies(id)
+    if cookies[:saved_products].nil?
+      cookies[:saved_products] = JSON.generate([id])
+    else
+      if !cookies[:saved_products].include?(id)
+        current = JSON.parse cookies[:saved_products]
+        current << id
+        cookies[:saved_products] = JSON.generate current
+      end
+    end
   end
 end
