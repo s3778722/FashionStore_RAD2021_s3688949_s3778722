ced8dd89e9678d462bd9915ce969b140623e33f0
s3688949@student.rmit.edu.au <s3688949@student.rmit.edu.au>
2021-05-23 21:53:02 +0000

    more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,7 @@ c2 = Category.create(title: "Women")
 c3 = Category.create(title: "Kids")
 
 u1 = User.create(username: "admin1", password: "adminadmin", password_confirmation: "adminadmin", email: "admin1@gmail.com")
+u2 = User.create(username: "RAD", password: "Rails2021", password_confirmation: "Rails2021", email: "rad2021rmit@gmail.com")
 p1 = Product.create(title: 'Short Sleeve Knit Polo', desc: 
     "Crafted from light and breathable cotton pique and finished with a classic flat knit collar and two-button placket, the Short Sleeve Knit Polo Shirt by Polo Ralph Lauren is a US patriot's dream. We love the unmistakable pony logo embroidered to the chest for a nod to the brand's sporting heritage.
 
@@ -137,4 +138,74 @@ p5.images.create(url: "https://kevymusicapp.s3.amazonaws.com/SmockMiniDress3.jpg
 p5.product_variants.create(size: "S", color: "Pink")
 p5.product_variants.create(size: "M", color: "Pink")
 
+p6 = Product.create(title: 'Blue Crush T-Shirt', desc: 
+    "Rock Your Kid delivers fun and fresh collections of kidswear your little one's bound to love. Playful prints and whimsical patterns sit alongside urban-inspired basics and outerwear, creating the perfect foundation for stylish kids who aren't afraid to be bold and stand out.
 
+    - Length: 53cm (size 8 years)
+    - Soft cotton jersey blend; slight stretch
+    - Tie dye print
+    - Round neckline", price: 36.00, date_stocked: DateTime.new(2020, 3, 11), popularity: 1)
+p6.collections.create(title: "T-Shirt")
+p6.collections.create(title: "Summer")
+p6.categories << c3
+p6.images.create(url: "https://kevymusicapp.s3.amazonaws.com/shirt1.jpg")
+p6.images.create(url: "https://kevymusicapp.s3.amazonaws.com/shirt2.jpg")
+p6.product_variants.create(size: "M", color: "Blue")
+p6.product_variants.create(size: "L", color: "Blue")
+
+p7 = Product.create(title: 'Payton Wide Leg Pant', desc: 
+    "Full of nostalgia, and crafted with a sustainable approach. Gelati Jeans is bringing teen girls across the country the latest trends. Apparently skinny legs are cancelled, so we're making sure she's got the comfiest wide leg style's this winter. A high-waisted style with pockets and raw edge hems, Payton has been crafted in Melbourne from premium cotton. Hand dyed to a lovely lilac and made to match the Payton Crop Sweat.
+    
+    Model is wearing a size L. She is 173.0cm (5'67”) tall with a 81cm (32”) bust, a 61cm (24”) waist and 89cm (35”) hips.
+    
+    - Length: 98 cm (Size M)
+    - High waisted, elastic waistband, full length, wide leg.
+    - Light-weight premium unbrushed cotton fleece
+    - Lilac purple shade; opaque; hand garment dyed in Melbourne
+    - Made in Australia", price: 69.00, date_stocked: DateTime.new(2021, 1, 3), popularity: 1)
+p7.collections.create(title: "Pants")
+p7.collections.create(title: "Premium")
+p7.categories << c3
+p7.images.create(url: "https://kevymusicapp.s3.amazonaws.com/trackies.jpg")
+p7.images.create(url: "https://kevymusicapp.s3.amazonaws.com/trackies2.jpg")
+p7.images.create(url: "https://kevymusicapp.s3.amazonaws.com/trackies3.jpg")
+p7.product_variants.create(size: "S", color: "Lavender")
+p7.product_variants.create(size: "M", color: "Lavender")
+p7.product_variants.create(size: "L", color: "Lavender")
+
+p8 = Product.create(title: 'Disney Comfy Princess Tights', desc: 
+    "The perfect fusion of form and function, adidas Performance blends cutting edge designs and technical fabrics with the latest trends to create activewear with a difference. 
+    
+    Whatever your sport, adidas Performance has the design for you, with an extensive range of tops, tights, shorts and performance shoes, as well as a broad offering of accessories.
+    
+    - Length: Inside Leg: 56cm. Rise: 15cm. Leg Opening: 20cm (size 7-8 years)
+    - Performance-blend double knit; super stretchy; opaque
+    - Elasticised waistband
+    - All-over Disney print", price: 55.00, date_stocked: DateTime.new(2021, 4, 3), popularity: 1)
+p8.collections.create(title: "Pants")
+p8.collections.create(title: "Sports")
+p8.categories << c3
+p8.images.create(url: "https://kevymusicapp.s3.amazonaws.com/adidas.jpg")
+p8.images.create(url: "https://kevymusicapp.s3.amazonaws.com/adidas2.jpg")
+p8.product_variants.create(size: "S", color: "Multi")
+p8.product_variants.create(size: "M", color: "Multi")
+
+p9 = Product.create(title: 'Penelope Long Sleeve Tee', desc: 
+    "As part of the great Cotton On kingdom, Cotton On Kids is a one-stop-shop for high value BABY, KIDS and TWEEN collections designed for active, growing bodies to live and play in throughout the year.
+
+    - Length: 55cm (size 8 years)
+    - Pure cotton jersey; slight stretch; opaque
+    - Graphic illustration print to chest", price: 16.99, date_stocked: DateTime.new(2020, 11, 9), popularity: 1)
+p9.collections.create(title: "Long Sleeves")
+p9.categories << c3
+p9.images.create(url: "https://kevymusicapp.s3.amazonaws.com/kidslong.jpg")
+p9.images.create(url: "https://kevymusicapp.s3.amazonaws.com/kidslong2.jpg")
+p9.product_variants.create(size: "S", color: "Purple")
+p9.product_variants.create(size: "M", color: "Purple")
+p9.product_variants.create(size: "L", color: "Purple")
+p9.product_variants.create(size: "S", color: "Peach")
+p9.product_variants.create(size: "M", color: "Peach")
+p9.product_variants.create(size: "L", color: "Peach")
+p9.product_variants.create(size: "S", color: "White")
+p9.product_variants.create(size: "M", color: "White")
+p9.product_variants.create(size: "L", color: "White")
